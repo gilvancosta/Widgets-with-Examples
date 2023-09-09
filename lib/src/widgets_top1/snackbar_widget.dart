c38a831c06677add129608587c2e0fefc114c80a
Gilvan Costa <gilvancosta2025@gmail.com>
2023-09-09 09:53:06 -0300

    adicionando snackbar

--- lib/src/widgets_top1/snackbar_widget.dart
+++ lib/src/widgets_top1/snackbar_widget.dart
@@ -1,14 +1,51 @@
 import 'package:flutter/material.dart';
 
 class SnackbarWidget extends StatelessWidget {
+  const SnackbarWidget({super.key});
 
-  const SnackbarWidget({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Snackbar Widget'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Snackbar Page'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    final snackBar = SnackBar(
+                      content: const Text('Hello SnackBar!'),
+                      backgroundColor: Colors.amber,
+                      action: SnackBarAction(
+                        label: 'Concordo',
+                        onPressed: () {
+                          // Some code to undo the change.
+                        },
+                      ),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  },
+                  child: const Text('Show SnackBar')),
+              ElevatedButton(
+                  onPressed: () {
+                    final snackBar = SnackBar(
+                      content: const Text('User registered successfully!'),
+                      action: SnackBarAction(
+                        label: 'Agree',
+                        onPressed: () {
+                          // Some code to undo the change.
+                        },
+                      ),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  },
+                  child: const Text('SnackBarAction')),
+            ],
+          ),
+        ));
   }
 }
